6d5bff6c3d884e6bc64b51b47095cabb05b4fe6b
Abubacer <azizdesignislife@gmail.com>
2023-10-06 01:56:32 +0000

    Deploy!

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -18,11 +18,11 @@ def do_pack():
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    file = f"versions/web_static_{timestamp}.tgz"
+    file_ = f"versions/web_static_{timestamp}.tgz"
 
-    dest_file = local(f"sudo tar -cvzf {file} web_static")
+    dest_file = local(f"sudo tar -cvzf {file_} web_static")
 
     if dest_file.succeeded:
-        return file
+        return file_
     else:
         return None
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,69 +3,38 @@
 This a Fabric script that deploys a .tgz archive to my web servers.
 """
 
+
+from fabric.api import *
+from datetime import datetime
 from os.path import exists
-from fabric.api import local, run, put, env
 
 
 env.hosts = ['3.84.161.155', '100.25.23.133']
 
 
 def do_deploy(archive_path):
+    """ distributes an archive to my web servers
     """
-    Distributes an archive to my web servers.
-
-    Args:
-        archive_path (str): The local path to the archive to be deployed.
-
-    Returns:
-        bool: True if all ops have been done correctly, otherwise False.
-    """
-    # Check if the archive file exists locally
     if exists(archive_path) is False:
         return False
 
-    # Get the file name without extension
-    file = archive_path.split('/')[-1]
-    file_name = file.split(".")[0]
+    file_ = archive_path.split('/')[-1]
+    # so now filename is <web_static_2021041409349.tgz>
+    no_tgz = '/data/web_static/releases/' + "{}".format(file_.split('.')[0])
+    # curr = '/data/web_static/current'
+    tmp = "/tmp/" + file_
 
     try:
-        # Upload the archive on the tmp directory in server.
-        put(archive_path, "/tmp/{}".format(file))
-
-        # Delete the existing directory for the new version, if it exists
-        run(f"rm -rf /data/web_static/releases/{file_name}/")
-
-        # Create the directory for the new version
-        run(f"mkdir -p /data/web_static/releases/{file_name}/")
-
-        # Uncompress the archive into the new directory
-        run(
-            f"tar -xzf /tmp/{file} -C /data//web_static/releases/{file_name}/"
-        )
-
-        # Remove the uploaded archive from the the tmp directory in server
-        run(f"rm /tmp/{file}")
-
-        # Move the contents to the correct location
-        run(
-            f"mv /data/web_static/releases/{file_name}/web_static/* "
-            f"/data/web_static/releases/{file_name}/"
-        )
-
-        # Delete the old web_static directory
-        run(
-            f"rm -rf /data/web_static/releases/{file_name}/web_static"
-        )
-
-        # Delete the old symbolic link
-        run(f"rm -rf /data/web_static/current")
-
-        # Create a new symbolic link to point the new version
-        run(
-            f"ln -s /data/web_static/releases/{file_name}/ "
-            f"/data/web_static/current"
-        )
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
 
         return True
-    except Exception as e:
+    except:
         return False
+
