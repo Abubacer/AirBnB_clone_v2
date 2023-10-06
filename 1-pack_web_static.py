df93efc40c38e083d8b9a1c12ab1ec238bffc2da
Abubacer <azizdesignislife@gmail.com>
2023-10-06 02:01:24 +0000

    Deploy it!

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -18,11 +18,11 @@ def do_pack():
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    file_ = f"versions/web_static_{timestamp}.tgz"
+    file = f"versions/web_static_{timestamp}.tgz"
 
-    dest_file = local(f"sudo tar -cvzf {file_} web_static")
+    dest_file = local(f"sudo tar -cvzf {file} web_static")
 
     if dest_file.succeeded:
-        return file_
+        return file
     else:
         return None
