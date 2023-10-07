fe02dd9579d20d22ed8af38c7f741ea14bd9fc7f
Abubacer <azizdesignislife@gmail.com>
2023-10-07 01:48:43 +0000

    D

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,9 @@
 """ This a Fabric script that deploys a .tgz archive to my web servers. """
 
 import os
-from fabric.api import local, run, put, env
+from datetime import datetime
+from fabric.api import local, run, put, env, runs_once
+
 
 # All remote commands are executed on my both web servers
 env.hosts = ['3.84.161.155', '100.25.23.133']
@@ -33,15 +35,15 @@ def do_deploy(archive_path):
         # Uncompress the archive into the new directory on the server
         run(f"tar -xzf /tmp/{file_name} -C {dir_path}")
         # Remove the tmp archive from the server
-        run(f"rm /tmp/{file_name}")
+        run(f"rm -rf /tmp/{file_name}")
         # Move the contents to the correct location
-        run(f"mv {dir_path}/web_static/* {dir_path}")
+        run(f"mv -n {dir_path}web_static/* {dir_path}")
         # Delete the old web_static directory
-        run(f"rm -rf {dir_path}/web_static")
+        run(f"rm -rf {dir_path}web_static")
         # Delete the old symbolic link
         run(f"rm -rf /data/web_static/current")
         # Create a new symbolic link to point the new version
-        run(f"ln -s {dir_path}/ /data/web_static/current")
+        run(f"ln -s {dir_path} /data/web_static/current")
 
         print("New version deployed!")
         return True
